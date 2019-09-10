800fbb026b9665b6fbff06030f9fdcb3599a7619
Michael <mr.enviro@gmail.com>
2019-09-10 23:03:43 +0000

    printing nodes and playing with stacking

--- linked-list-1.rb
+++ linked-list-1.rb
@@ -1,16 +1,72 @@
 
+# Linked list Class
+class LinkedListNode
+	attr_accessor :value, :next_node
 
+	def initialize(value, next_node=nil)
+		@value = value
+		@next_node = next_node
+	end
+end
 
+# Adding to Linked list
+node1 = LinkedListNode.new(27)
+node2 = LinkedListNode.new(89, node1)
+node3 = LinkedListNode.new(22, node2)
 
+# Print function
+def print_values(list_node)
+	if list_node
+		print "#{list_node.value} --> "
+		print_values(list_node.next_node)
+	else
+		print "nil\n"
+		return
+	end
+end
 
-
-
-
-
+# Call Statement
 print_values(node3)
+puts "----------------------"
 
-puts "-------"
 
-revlist = reverse_list(node3)
+# Stack Class
+class Stack
+	attr_reader :data
 
-print_values(revlist)
+	def initialize
+		@data = nil
+	end
+
+	def push(value)
+		@data = LinkedListNode.new(value, @data)
+	end
+
+	def pop
+		return print "nil\n" if @data.nil?
+		print "#{@data.value}\n"
+		@data = @data.next_node
+	end
+end
+
+# Create a stack
+stack = Stack.new
+
+# Adding to stack
+stack.push(33)
+stack.push(44)
+stack.push(55)
+# Iterative approach
+10.times do |i|
+	stack.push(i + 1)
+end
+
+
+# Removing from stack
+stack.pop 
+stack.pop
+stack.pop
+# Iterative approach
+12.times do
+	stack.pop
+end	
