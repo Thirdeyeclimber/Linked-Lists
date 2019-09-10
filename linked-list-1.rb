427f531e2b30bf1bec76b7ff81c033d984238cf4
Michael <mr.enviro@gmail.com>
2019-09-10 23:23:04 +0000

    fix indentation issue

--- linked-list-1.rb
+++ linked-list-1.rb
@@ -1,12 +1,12 @@
 
 # Linked list Class
 class LinkedListNode
-	attr_accessor :value, :next_node
+  attr_accessor :value, :next_node
 
-	def initialize(value, next_node=nil)
-		@value = value
-		@next_node = next_node
-	end
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
 end
 
 # Adding to Linked list
@@ -16,33 +16,33 @@ node3 = LinkedListNode.new(22, node2)
 
 # Print function
 def print_values(list_node)
-	if list_node
-		print "#{list_node.value} --> "
-		print_values(list_node.next_node)
-	else
-		print "nil\n"
-		return
-	end
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
 end
 
 
 # Stack Class
 class Stack
-	attr_reader :data
+  attr_reader :data
 
-	def initialize
-		@data = nil
-	end
+  def initialize
+    @data = nil
+  end
 
-	def push(value)
-		@data = LinkedListNode.new(value, @data)
-	end
+  def push(value)
+    @data = LinkedListNode.new(value, @data)
+  end
 
-	def pop
-		return print "nil\n" if @data.nil?
-		print "#{@data.value}\n"
-		@data = @data.next_node
-	end
+  def pop
+    return print "nil\n" if @data.nil?
+    print "#{@data.value}\n"
+    @data = @data.next_node
+  end
 end
 
 # Create a new stack
@@ -54,7 +54,7 @@ stack.push("two")
 stack.push("three")
 # Iterative approach
 3.times do |i|
-	stack.push(i + 1)
+  stack.push(i + 1)
 end
 
 
@@ -64,19 +64,19 @@ stack.pop
 stack.pop
 # Iterative approach
 4.times do
-	stack.pop
-end	
+  stack.pop
+end 
 
 puts "---------------"
 
 
 def reverse_list(list)
-	stack = Stack.new
-	while list
-		stack.push(list.value)
-		list = list.next_node
-	end
-	return stack.data
+  stack = Stack.new
+  while list
+    stack.push(list.value)
+    list = list.next_node
+  end
+  return stack.data
 end
 
 print_values(node3)
